f58a21b45b68b1800077e4444bf1164db779b338
Jonas Wanke <contact@jonas-wanke.com>
2023-01-25 12:16:09 +0100

    Remove superfluous import

--- lib/src/components/multi_date_event_header.dart
+++ lib/src/components/multi_date_event_header.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:black_hole_flutter/black_hole_flutter.dart';
-import 'package:dartx/dartx.dart';
 import 'package:flutter/material.dart' hide Interval;
 import 'package:flutter/rendering.dart';
 

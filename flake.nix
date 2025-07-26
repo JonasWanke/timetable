16a721444cf9431af2c8285e0d57d46aafd1b227
Jonas Wanke <contact@jonas-wanke.com>
2025-07-26 17:37:56 +0200

    Upgrade to Flutter 3.32

--- flake.nix
+++ flake.nix
@@ -4,8 +4,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -15,37 +21,27 @@
           };
         };
 
-        flutter = pkgs.flutterPackages.v3_24;
+        flutter = pkgs.flutterPackages.v3_32;
 
         # Android
         androidSdkArgs = {
           buildToolsVersions = [ "30.0.3" ];
           platformVersions = [ "34" ];
         };
-        androidComposition =
-          pkgs.androidenv.composeAndroidPackages androidSdkArgs;
+        androidComposition = pkgs.androidenv.composeAndroidPackages androidSdkArgs;
         androidSdk = androidComposition.androidsdk;
-        androidEmulator = pkgs.androidenv.emulateApp {
-          name = "Emulator";
-          platformVersion = "34";
-          systemImageType = "google_apis_playstore";
-          abiVersion = "x86_64";
-          configOptions = {
-            # https://android.googlesource.com/platform/external/qemu/+/refs/heads/master/android/avd/hardware-properties.ini
-            "hw.ramSize" = "4096";
-            "hw.lcd.width" = "1170";
-            "hw.lcd.height" = "2532";
-            "hw.lcd.density" = "460";
-            "hw.keyboard" = "yes";
-          };
-          sdkExtraArgs = androidSdkArgs;
-        };
-      in {
-        devShell = with pkgs;
+      in
+      {
+        devShell =
+          with pkgs;
           mkShell {
             ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
             FLUTTER_ROOT = flutter;
-            buildInputs = [ androidEmulator androidSdk flutter ];
+            buildInputs = [
+              androidSdk
+              flutter
+            ];
           };
-      });
+      }
+    );
 }
